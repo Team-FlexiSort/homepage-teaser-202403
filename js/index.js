36915af393f3220ac39dc24c528a029ec9716046
Makoto Yamashita <makochan12.9@gmail.com>
2024-03-06 00:48:03 +0900

    smooth scroll

---
+++ js/index.js
@@ -0,0 +1,35 @@
+// Get the header element
+const header = document.querySelector('header');
+
+// Get the height of the header
+const headerHeight = header.offsetHeight;
+
+// Add smooth scroll behavior to all anchor links
+const anchorLinks = document.querySelectorAll('a[href^="#"]');
+anchorLinks.forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+
+        // Get the target element's ID from the href attribute
+        const targetId = link.getAttribute('href').slice(1);
+        if (targetId == 'top') {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        }
+        else {
+            // Get the target element
+            const targetElement = document.getElementById(targetId);
+
+            // Calculate the scroll position, considering the header height
+            const scrollPosition = targetElement.offsetTop - headerHeight;
+
+            // Scroll smoothly to the target element
+            window.scrollTo({
+                top: scrollPosition,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
